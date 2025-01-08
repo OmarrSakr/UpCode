d1e94bada6daad87b67e005a1ab1ba05265720b4
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:31:26 +0200

    Update 1_A_Theatre Square.cs

--- Codeforces_Solutions/1_A_Theatre Square/1_A_Theatre Square.cs
+++ Codeforces_Solutions/1_A_Theatre Square/1_A_Theatre Square.cs
@@ -1,1 +1,28 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Theatre_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+ 
+            long n = long.Parse(input[0]); // Length
+            long m = long.Parse(input[1]); //Width
+            long a = long.Parse(input[2]); // lenght and width of tiles البلاط .. لان طول والعرض للبلاط متساوي a*a
+ 
+            long tilesINLength = (n + a - 1) / a;
+            long tilesINWidth = (m + a - 1) / a;
+ 
+            long TotalTiles = tilesINLength * tilesINWidth;
+            Console.WriteLine(TotalTiles);
+ 
+       
+        }
+    }
+}
