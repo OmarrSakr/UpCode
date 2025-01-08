7bf96562f0231079c75d1c6e713b46d8e7f7706e
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:35:17 +0200

    Update 2042_A_Greedy Monocarp.cs

--- Codeforces_Solutions/2042_A_Greedy Monocarp/2042_A_Greedy Monocarp.cs
+++ Codeforces_Solutions/2042_A_Greedy Monocarp/2042_A_Greedy Monocarp.cs
@@ -1,1 +1,49 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Greedy_Monocarp
+{
+    class Program
+    {
+        static void ProcessTestCase()
+        {
+            int totalElements, threshold;
+            var inputs = Console.ReadLine().Split();
+            totalElements = int.Parse(inputs[0]);
+            threshold = int.Parse(inputs[1]);
+ 
+            // Read the elements into an array
+            var elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+ 
+            // Sort the array in descending order
+            Array.Sort(elements, (a, b) => b.CompareTo(a));
+ 
+            int accumulatedSum = 0;
+            for (int i = 0; i < totalElements; i++)
+            {
+                accumulatedSum += elements[i];
+                if (accumulatedSum > threshold)
+                {
+                    accumulatedSum -= elements[i];
+                    Console.WriteLine(threshold - accumulatedSum);
+                    return;
+                }
+            }
+ 
+            // If no sum exceeds the threshold, print the remaining sum
+            Console.WriteLine(threshold - accumulatedSum);
+        }
+ 
+        static void Main()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                ProcessTestCase();
+            }
+        }
+    }
+}
