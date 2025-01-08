67e675d5f7e25643a1642213c3aa59275cc1aba6
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:15:17 +0200

    Update 265_A_Colorful Stones (Simplified Edition).cs

--- Codeforces_Solutions/265_A_Colorful Stones (Simplified Edition)/265_A_Colorful Stones (Simplified Edition).cs
+++ Codeforces_Solutions/265_A_Colorful Stones (Simplified Edition)/265_A_Colorful Stones (Simplified Edition).cs
@@ -1,1 +1,32 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s=Console.ReadLine().ToUpper();
+            string t=Console.ReadLine().ToUpper();
+ 
+            int position = 0;
+            for (int i = 0; i < t.Length; i++)
+            {
+                if (t[i] == s[position])
+                {
+                    position++;
+                }
+                if (position==s.Length)
+                {
+                    break;
+                }
+ 
+            }
+            Console.WriteLine(position + 1);   // ( الموضع النهائي (مع زيادة 1 لأن الترقيم يبدأ من 1
+        }
+    }
+}
