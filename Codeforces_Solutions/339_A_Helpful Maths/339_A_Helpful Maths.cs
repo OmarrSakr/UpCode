46c4e3d2926280bb643ad67c33f04f7dda32750a
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:30:57 +0200

    Update 339_A_Helpful Maths.cs

--- Codeforces_Solutions/339_A_Helpful Maths/339_A_Helpful Maths.cs
+++ Codeforces_Solutions/339_A_Helpful Maths/339_A_Helpful Maths.cs
@@ -1,1 +1,22 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=Console.ReadLine();
+            string[] record = input.Split('+');
+           
+            Array.Sort(record);
+            string result=string.Join("+", record);
+            Console.WriteLine(result);
+        }
+    }
+}
