8b6804794e71cdacdea930edb7740f3a31973b1b
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:42:38 +0200

    Update 766_A_Mahmoud and Longest Uncommon Subsequence.cs

--- Codeforces_Solutions/766_A_Mahmoud and Longest Uncommon Subsequence/766_A_Mahmoud and Longest Uncommon Subsequence.cs
+++ Codeforces_Solutions/766_A_Mahmoud and Longest Uncommon Subsequence/766_A_Mahmoud and Longest Uncommon Subsequence.cs
@@ -1,1 +1,28 @@
-Code not available
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Mahmoud_and_Longest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string seqA = Console.ReadLine();
+            string seqB = Console.ReadLine();
+ 
+            if (seqA == seqB)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine(Math.Max(seqA.Length, seqB.Length));
+            } 
+ 
+        }
+    }
+}
