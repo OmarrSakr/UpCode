4c0317b9e2b3bc868f8b4608d43c2a52e94e38e2
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:39:11 +0200

    Update 791_A_Bear and Big Brother.cs

--- Codeforces_Solutions/791_A_Bear and Big Brother/791_A_Bear and Big Brother.cs
+++ Codeforces_Solutions/791_A_Bear and Big Brother/791_A_Bear and Big Brother.cs
@@ -1,1 +1,30 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+ 
+namespace cf3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] onlyline=Console.ReadLine().Split(' ');
+            int x=int.Parse(onlyline[0]);
+            int y=int.Parse(onlyline[1]);
+ 
+            int countyears = 0;
+ 
+            while(x <= y)
+            {
+                x *= 3;
+                y*=2;
+                countyears++;
+            }
+            Console.WriteLine(countyears);
+ 
+        }
+    }
+}
