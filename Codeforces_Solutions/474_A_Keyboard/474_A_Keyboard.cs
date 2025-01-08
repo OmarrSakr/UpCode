8fc5682a0862ae7fd69b61f537238c9daf62fefb
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:22:23 +0200

    Update 474_A_Keyboard.cs

--- Codeforces_Solutions/474_A_Keyboard/474_A_Keyboard.cs
+++ Codeforces_Solutions/474_A_Keyboard/474_A_Keyboard.cs
@@ -1,1 +1,53 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Keyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string value = "qwertyuiopasdfghjkl;zxcvbnm,./";
+            string LR=Console.ReadLine();
+            if(LR.Length!=1 || (LR!="L" && LR!="R"))
+            {
+                return;
+            }
+            LR = LR.ToUpper();
+ 
+            string input = Console.ReadLine();
+   
+ 
+            StringBuilder output = new StringBuilder();
+            foreach (char c in input)
+            {
+                int index = value.IndexOf(c);
+                if(index == -1)
+                {
+                    return;
+                }
+                if (LR == "R")
+                {
+                    if(index - 1 >= 0)
+                    {
+ 
+                      output.Append(value[index - 1]);
+                    }
+                }
+                else
+                {
+                    if (index + 1 < value.Length)
+                    {
+                        output.Append(value[index + 1]);
+                    }
+ 
+                }
+            }
+            Console.WriteLine(output.ToString());
+ 
+        }
+    }
+}
