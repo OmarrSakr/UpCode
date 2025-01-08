b31a488b31a78bb00a600917bc0f799055deee14
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:19:16 +0200

    Update 50_A_Domino piling.cs

--- Codeforces_Solutions/50_A_Domino piling/50_A_Domino piling.cs
+++ Codeforces_Solutions/50_A_Domino piling/50_A_Domino piling.cs
@@ -1,1 +1,23 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[]record=Console.ReadLine().Split(' ');
+            int M =int.Parse(record[0]); //M N these are dimensions
+            int N =int.Parse(record[1]);
+ 
+            int maxDominoes = (M * N)/2 ;
+ 
+            Console.WriteLine(maxDominoes);
+ 
+        }
+    }
+}
