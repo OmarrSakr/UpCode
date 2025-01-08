653c18d90b206c29d0a453f1563b8be54e457f75
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:28:23 +0200

    Update 136_A_Presents.cs

--- Codeforces_Solutions/136_A_Presents/136_A_Presents.cs
+++ Codeforces_Solutions/136_A_Presents/136_A_Presents.cs
@@ -1,1 +1,27 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Presents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] P= Console.ReadLine().Split();
+ 
+            // تخزين الأرقام في مصفوفة أو طباعتها
+            int[] result = new int[n];
+ 
+            for (int i = 0; i < n; i++)
+            {
+                int friendNumbers = int.Parse(P[i]);
+                result[friendNumbers-1] = i+1;
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
