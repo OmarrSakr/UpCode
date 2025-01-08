97e517abd90883af69917deecdc91cfc1f60e01a
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:45:27 +0200

    Update 732_A_Buy a Shovel.cs

--- Codeforces_Solutions/732_A_Buy a Shovel/732_A_Buy a Shovel.cs
+++ Codeforces_Solutions/732_A_Buy a Shovel/732_A_Buy a Shovel.cs
@@ -1,1 +1,31 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[]record=Console.ReadLine().Split(' ');
+            int k=int.Parse(record[0]);  
+            int r=int.Parse(record[1]);   
+            int totalcost;                      // numbers of shovels
+            for ( int n=1;n<=10; n++)
+            {
+                 totalcost = k * n;
+                if (totalcost % 10 == r || totalcost % 10 == 0)
+                {
+ 
+                    Console.WriteLine(n);
+                    return;
+                }
+            }
+         
+ 
+        }
+    }
+}
