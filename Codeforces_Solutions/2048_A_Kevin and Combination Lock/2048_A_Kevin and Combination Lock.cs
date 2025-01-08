379c134bcbb620319d360f52b2678031ba2cdeba
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:02:52 +0200

    Update 2048_A_Kevin and Combination Lock.cs

--- Codeforces_Solutions/2048_A_Kevin and Combination Lock/2048_A_Kevin and Combination Lock.cs
+++ Codeforces_Solutions/2048_A_Kevin and Combination Lock/2048_A_Kevin and Combination Lock.cs
@@ -1,1 +1,48 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Kevin_and_Combination_Lock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine()); // عدد حالات الاختبار
+            for (int i = 0; i < t; i++)
+            {
+                long x = long.Parse(Console.ReadLine()); // القيمة الأولية للـ x
+ 
+                // الحل:
+                // إذا كان العدد x يقبل القسمة على 33، فإن الجواب هو "YES"
+                // وإلا إذا كان يحتوي على "33"، نزيلهما حتى نصل إلى الحل أو لا.
+                if (x % 33 == 0)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    string xStr = x.ToString();
+                    bool possible = false;
+ 
+                    while (xStr.Contains("33"))
+                    {
+                        xStr = xStr.Replace("33", ""); // إزالة أي "33" موجودة في الرقم
+                        if (string.IsNullOrEmpty(xStr) || xStr == "0")
+                        {
+                            possible = true;
+                            break;
+                        }
+                    }
+ 
+                    if (possible)
+                        Console.WriteLine("YES");
+                    else
+                        Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
