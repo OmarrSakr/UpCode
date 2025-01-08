0c422d8f52b1147ab06bf4be985468ca41dd1525
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:24:43 +0200

    Update 43_A_Football.cs

--- Codeforces_Solutions/43_A_Football/43_A_Football.cs
+++ Codeforces_Solutions/43_A_Football/43_A_Football.cs
@@ -1,1 +1,45 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Football
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+ 
+            Dictionary<string, int> goals = new Dictionary<string, int>();
+            for(int i = 1; i <= n; i++)
+            {
+                string teamName = Console.ReadLine();
+ 
+                if (goals.ContainsKey(teamName))
+                {
+                    goals[teamName]++;
+                }
+                else
+                {
+                    goals[teamName] = 1;
+                }
+            }
+ 
+            string Winner ="";
+            int Maxgoals = 0;
+            foreach(var team in goals)
+            {
+                if(team.Value > Maxgoals)
+                {
+                    Winner = team.Key;
+                    Maxgoals = team.Value;
+                }
+            }
+            Console.WriteLine(Winner);
+            
+ 
+        }
+    }
+}
