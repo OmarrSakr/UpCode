787c0b4f73a8697a5196347be012a4632b68fb32
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:40:47 +0200

    Update 770_A_New Password.cs

--- Codeforces_Solutions/770_A_New Password/770_A_New Password.cs
+++ Codeforces_Solutions/770_A_New Password/770_A_New Password.cs
@@ -1,1 +1,24 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace New_Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split();
+            int n = int.Parse(inputs[0]);
+            int k = int.Parse(inputs[1]);
+ 
+            for (int i = 0; i < n; i++)
+            {
+                char ch = (char)('a' + (i % k));
+                Console.Write(ch);
+            }
+        }
+    }
+}
