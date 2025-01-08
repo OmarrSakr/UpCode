bc173ba1190466d58df6c9e21199520f9f2fba03
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:37:48 +0200

    Update 807_A_Is it rated_.cs

--- Codeforces_Solutions/807_A_Is it rated_/807_A_Is it rated_.cs
+++ Codeforces_Solutions/807_A_Is it rated_/807_A_Is it rated_.cs
@@ -1,1 +1,55 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Is_it_rated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int participants = int.Parse(Console.ReadLine()); // n
+            int[] ai = new int[participants];
+            int[] bi = new int[participants];
+ 
+            bool isRated = false;
+            bool isUnrated = false;
+ 
+            for(int i = 0; i < participants; i++)
+            {
+            string[] input = Console.ReadLine().Split();
+                 ai[i] = int.Parse(input[0]);
+                 bi[i] = int.Parse(input[1]);
+ 
+                if(ai[i] != bi[i])
+                {
+                    isRated = true;
+                }
+            }
+            if (isRated)
+            {
+                Console.WriteLine("rated");
+                return;
+            }
+            for (int i = 1; i < participants; i++)
+            {
+                if (ai[i] > ai[i - 1])
+                {
+                    isUnrated = true;
+                    break;
+                }
+            }
+ 
+                if (isUnrated)
+            {
+                Console.WriteLine("unrated");
+            }
+            else
+            {
+                Console.WriteLine("maybe");
+            }
+        }
+    }
+}
