ccc757dfb24ca23e92c5704802ed1c3244a5ca9a
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:22:59 +0200

    Update 469_A_I Wanna Be the Guy.cs

--- Codeforces_Solutions/469_A_I Wanna Be the Guy/469_A_I Wanna Be the Guy.cs
+++ Codeforces_Solutions/469_A_I Wanna Be the Guy/469_A_I Wanna Be the Guy.cs
@@ -1,1 +1,33 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.I_Wanna_Be_the_Guy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int levels = int.Parse(Console.ReadLine());  // n
+            string[] indicateX = Console.ReadLine().Split();
+            string[] indicateY = Console.ReadLine().Split();
+ 
+            List<int> Xlevels = indicateX.Skip(1).Select(int.Parse).ToList();
+            List<int> Ylevels = indicateY.Skip(1).Select(int.Parse).ToList();
+ 
+ 
+            var AllLevels = Xlevels.Concat(Ylevels).ToHashSet();
+ 
+            if (AllLevels.Count == levels)
+            {
+                Console.WriteLine("I become the guy.");
+            }
+            else
+            {
+                Console.WriteLine("Oh, my keyboard!");
+            }
+        }
+    }
+}
