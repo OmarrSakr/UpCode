a8803be92d38f39fb8be133bd58f9d75cb41c97a
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:38:34 +0200

    Update 799_A_Carrot Cakes.cs

--- Codeforces_Solutions/799_A_Carrot Cakes/799_A_Carrot Cakes.cs
+++ Codeforces_Solutions/799_A_Carrot Cakes/799_A_Carrot Cakes.cs
@@ -1,1 +1,33 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string[]record=Console.ReadLine().Split(' ');
+ 
+            int n=int.Parse(record[0]);  // Number of (cakes) for complete Task
+            int t=int.Parse(record[1]);  // Number of (minutes) for number of (K)
+            int k=int.Parse(record[2]);   // (K) Number of Cakes Boke in the Limit time is (t) 
+            int d=int.Parse(record[3]);   // Numbers of (minutes) we need for do (the second Oven)
+ 
+          int r=(d+t)/ t; // ( r ) represent rate time of two ovens
+ 
+            if(r * k < n)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+ 
+        }
+    }
+}
