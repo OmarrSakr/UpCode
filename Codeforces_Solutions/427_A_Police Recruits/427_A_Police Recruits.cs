7dabbe159c3e40c85c396e3a20c9f1775c4773e5
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:26:06 +0200

    Update 427_A_Police Recruits.cs

--- Codeforces_Solutions/427_A_Police Recruits/427_A_Police Recruits.cs
+++ Codeforces_Solutions/427_A_Police Recruits/427_A_Police Recruits.cs
@@ -1,1 +1,44 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            string[] record=Console.ReadLine().Split(' ');
+            int[]occur=new int[n];
+            for(int i=0; i<n; i++)
+            {
+                occur[i]=int.Parse(record[i]);  
+            }
+            int untreated = 0;
+            int availableofficers = 0;
+            for(int i=0; i<n; i++)
+            {
+               if( occur[i] == -1)     // the first step
+                {
+                   if(availableofficers >0)
+                    {
+                        availableofficers--;
+                    }
+                    else
+                    {
+                        untreated++;
+                    }
+                }
+                else                          //the second step
+                {
+                    availableofficers=availableofficers+occur[i];
+                }
+            }
+            Console.WriteLine(untreated);
+ 
+        }
+    }
+}
