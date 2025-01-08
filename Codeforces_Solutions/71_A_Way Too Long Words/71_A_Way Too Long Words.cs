03bf859ea6a8981ce2b18f55e1a500b367f8f33d
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:53:49 +0200

    Update 71_A_Way Too Long Words.cs

--- Codeforces_Solutions/71_A_Way Too Long Words/71_A_Way Too Long Words.cs
+++ Codeforces_Solutions/71_A_Way Too Long Words/71_A_Way Too Long Words.cs
@@ -1,1 +1,39 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            string[] words = new string[n];
+            int count = 0;
+            for(int i=0; i<n;i++)
+            {
+                words[i] = Console.ReadLine();
+ 
+            }
+            for(int i=0; i<n; i++)
+            {
+                if(words[i].Length > 10)
+                {
+ 
+                    count = words[i].Length - 2;  // حساب عدد الأحرف بين الأول والأخير
+                    Console.WriteLine($"{words[i][0]}{count}{words[i][words[i].Length - 1]}");
+                }
+                else                             // إذا كانت الكلمة ليست أطول من 10 أحرف
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+         
+ 
+        }
+    }
+}
