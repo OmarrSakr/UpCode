ee26fae58e8af82fd925dfbaf66d8526561d8168
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:20:19 +0200

    Update 486_A_Calculating Function.cs

--- Codeforces_Solutions/486_A_Calculating Function/486_A_Calculating Function.cs
+++ Codeforces_Solutions/486_A_Calculating Function/486_A_Calculating Function.cs
@@ -1,1 +1,23 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Calculating_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            // سؤال دا عبارة عن سلسلة من
+            // (-1)^n *n 
+            // يعني او n=2 
+            //(-1)^1 *1 + (-1)^2 *2
+            
+            long result = (n % 2 == 0) ? (n/2) : -(n+1)/2;
+            Console.WriteLine(result);
+        }
+    }
+}
