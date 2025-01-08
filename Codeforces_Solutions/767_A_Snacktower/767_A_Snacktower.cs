b74d0d8f7945b6f53754f10b1f5695fe422239b6
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:42:02 +0200

    Update 767_A_Snacktower.cs

--- Codeforces_Solutions/767_A_Snacktower/767_A_Snacktower.cs
+++ Codeforces_Solutions/767_A_Snacktower/767_A_Snacktower.cs
@@ -1,1 +1,40 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Snacktower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] P = Console.ReadLine().Split();
+ 
+            int[] snackSizes = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                 snackSizes[i] = int.Parse(P[i]);
+            }
+ 
+            int expected = n;
+            // مصفوفة لتخزين الوجبات التي تم وضعها في البرج
+            HashSet<int> waitingSnacks = new HashSet<int>();
+            for (int i = 0; i < n; i++)
+            {
+                waitingSnacks.Add(snackSizes[i]);
+ 
+                while (waitingSnacks.Contains(expected))
+                {
+                    Console.Write(expected + " ");
+                    waitingSnacks.Remove(expected);
+                    expected--;
+                }
+                    Console.WriteLine();
+            }
+ 
+        }
+    }
+}
