cc163fc8d169031d7564474b25e447599c8e1aeb
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:30:04 +0200

    Update 344_A_Magnets.cs

--- Codeforces_Solutions/344_A_Magnets/344_A_Magnets.cs
+++ Codeforces_Solutions/344_A_Magnets/344_A_Magnets.cs
@@ -1,1 +1,34 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int n =int.Parse(Console.ReadLine());
+            string[]magnet=new string[n];
+            int group = 1;                       // 
+            for (int i = 0; i < n; i++)
+            {
+                magnet[i]=Console.ReadLine();
+ 
+            }
+           
+            for (int i = 1; i < n; i++)
+            {                                    //0  1  2   3   4  5
+                if (magnet[i] != magnet[i-1])   //10 10 10 (01) 10 10
+                {
+                    group++;
+                }
+            }
+            Console.WriteLine(group);
+ 
+ 
+        }
+    }
+}
