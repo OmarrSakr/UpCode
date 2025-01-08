ba1c44f0b06b9394bfab0809f9b7842029bf05c9
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:59:10 +0200

    Update 584_A_Olesya and Rodion.cs

--- Codeforces_Solutions/584_A_Olesya and Rodion/584_A_Olesya and Rodion.cs
+++ Codeforces_Solutions/584_A_Olesya and Rodion/584_A_Olesya and Rodion.cs
@@ -1,1 +1,36 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Olesya_and_Rodion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]); // n=3 or(1 ≤ n ≤ 100) => any number consist of 3 digits
+            int t = int.Parse(input[1]);
+ 
+            // If t is 10 and n is 1, it's impossible to construct such a number
+            if (n==1 && t == 10)
+            {
+                Console.WriteLine("-1");
+                return;
+            }
+ 
+            if(t == 10)
+            {
+                Console.WriteLine("1" + new string('0', n - 1));
+            }
+            else
+            {
+                Console.WriteLine(t + new string('0', n - 1));
+ 
+            }
+ 
+        }
+    }
+}
