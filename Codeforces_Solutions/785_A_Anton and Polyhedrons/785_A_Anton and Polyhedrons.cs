b144b659d12e37b9016f17b5619ce3db9bb8e82a
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:40:09 +0200

    Update 785_A_Anton and Polyhedrons.cs

--- Codeforces_Solutions/785_A_Anton and Polyhedrons/785_A_Anton and Polyhedrons.cs
+++ Codeforces_Solutions/785_A_Anton and Polyhedrons/785_A_Anton and Polyhedrons.cs
@@ -1,1 +1,35 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Anton_and_Polyhedrons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int n = int.Parse(Console.ReadLine());
+            long result = 0;
+ 
+            // Mapping polyhedron names to their face counts
+            Dictionary<string, int> faceCounts = new Dictionary<string, int>
+            {
+                { "Tetrahedron", 4 },
+                { "Cube", 6 },
+                { "Octahedron", 8 },
+                { "Dodecahedron", 12 },
+                { "Icosahedron", 20 }
+            };
+ 
+            for (int i = 0; i < n; i++)
+            {
+                string polyhedron = Console.ReadLine();
+                result += faceCounts[polyhedron];
+            }
+ 
+            Console.WriteLine(result);
+        }
+    }
+}
