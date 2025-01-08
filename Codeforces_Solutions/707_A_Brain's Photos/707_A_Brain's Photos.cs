625cb40086e6df47c5d506503523ae8d14e9758f
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:55:10 +0200

    Update 707_A_Brain's Photos.cs

--- Codeforces_Solutions/707_A_Brain's Photos/707_A_Brain's Photos.cs
+++ Codeforces_Solutions/707_A_Brain's Photos/707_A_Brain's Photos.cs
@@ -1,1 +1,40 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Brain_s_Photos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+ 
+ 
+            bool isColor = false;
+ 
+            for(int i = 0; i < n; i++)
+            {
+                string[] row = Console.ReadLine().Split();
+ 
+                foreach(string pixel in row)
+                {
+                    if (pixel == "C" || pixel == "M" || pixel == "Y")
+                    {
+                        isColor = true;
+                        break;
+                    }
+                    if(isColor)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(isColor? "#Color" : "#Black&White");
+        }
+    }
+}
