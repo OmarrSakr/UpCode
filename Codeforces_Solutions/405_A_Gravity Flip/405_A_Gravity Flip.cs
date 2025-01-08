ee2524af1debe0a881a68cf0a42d1f8675da89d9
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:26:43 +0200

    Update 405_A_Gravity Flip.cs

--- Codeforces_Solutions/405_A_Gravity Flip/405_A_Gravity Flip.cs
+++ Codeforces_Solutions/405_A_Gravity Flip/405_A_Gravity Flip.cs
@@ -1,1 +1,28 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            int[] column = new int[n];
+ 
+            string[] firstline = Console.ReadLine().Split(' ');
+            for(int i = 0;i<n;i++)
+            {
+                column[i] = int.Parse(firstline[i]);
+               
+            }
+            Array.Sort(column);
+            Console.WriteLine(string.Join(" ",column));
+ 
+ 
+        }
+    }
+}
