7a18879e76aef308f61c1b031dfe862ac6935db9
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:23:59 +0200

    Update 443_A_Anton and Letters.cs

--- Codeforces_Solutions/443_A_Anton and Letters/443_A_Anton and Letters.cs
+++ Codeforces_Solutions/443_A_Anton and Letters/443_A_Anton and Letters.cs
@@ -1,1 +1,35 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input=Console.ReadLine();   // // قراءة السلسلة المدخلة والتي تتضمن الأقواس User هو اللي هيدخلها كلها 
+            input=input.Substring(1,input.Length-2);
+            if(string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("0");
+                return;
+            }
+ 
+            string[] record = input.Split(',');
+            HashSet<char> uniqueletters=new HashSet<char>();
+            foreach (char c in input)
+            {
+                if (c != ' ' && c != ',')
+                {
+                  uniqueletters.Add(c);
+                }
+            }
+          
+                Console.WriteLine(uniqueletters.Count);
+          
+        }
+    }
+}
