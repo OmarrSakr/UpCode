82a72dc9dd5e7b9488f7c344402022c7d541cec1
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:44:10 +0200

    Update 734_A_Anton and Danik.cs

--- Codeforces_Solutions/734_A_Anton and Danik/734_A_Anton and Danik.cs
+++ Codeforces_Solutions/734_A_Anton and Danik/734_A_Anton and Danik.cs
@@ -1,1 +1,44 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            string s = Console.ReadLine();
+            int countA = 0, countD = 0;
+ 
+            for (int i = 0;i < n; i++)
+            {
+                if(s[i]=='A')
+                {
+                    countA++;
+                }
+                else
+                {
+                    countD++;
+                }
+            }
+            if(countA > countD)
+            {
+                Console.WriteLine("Anton");
+            }
+            else if(countA < countD)
+            {
+                Console.WriteLine("Danik");
+            }
+            else
+            {
+                Console.WriteLine("Friendship");
+            }
+ 
+ 
+        }
+    }
+}
