93d91a8c78c8426835a5574e63be6d9357e7748c
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:12:56 +0200

    Update 231_A_Team.cs

--- Codeforces_Solutions/231_A_Team/231_A_Team.cs
+++ Codeforces_Solutions/231_A_Team/231_A_Team.cs
@@ -1,1 +1,36 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for(int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                string[]inputs = Console.ReadLine().Split(' ');
+ 
+                int petya = int.Parse(inputs[0]);
+                int vasya = int.Parse(inputs[1]);
+                int tonya = int.Parse(inputs[2]);
+ 
+                 sum = petya + vasya + tonya;
+ 
+                if(sum>=2)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+ 
+ 
+        }
+  }
+}
