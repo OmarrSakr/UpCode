a3981e736b6f931954de08a51f4d56b2aa26e4c4
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:58:32 +0200

    Update 59_A_Word.cs

--- Codeforces_Solutions/59_A_Word/59_A_Word.cs
+++ Codeforces_Solutions/59_A_Word/59_A_Word.cs
@@ -1,1 +1,39 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int countUpper = 0; int countLower = 0;
+            foreach (char c in s)
+            {
+                if (Char.IsUpper(c))
+                {
+                    countUpper++;
+                }
+                else
+                {
+                    countLower++;
+                }
+            }
+ 
+            if (countUpper > countLower)
+            {
+                Console.WriteLine(s.ToUpper());
+            }
+            else
+            {
+                Console.WriteLine(s.ToLower());
+            }
+ 
+        }
+    }
+}
