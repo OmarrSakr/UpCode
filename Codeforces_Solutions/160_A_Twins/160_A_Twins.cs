fc0870863cda95bba4342018174a214070e77c41
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:30:34 +0200

    Update 160_A_Twins.cs

--- Codeforces_Solutions/160_A_Twins/160_A_Twins.cs
+++ Codeforces_Solutions/160_A_Twins/160_A_Twins.cs
@@ -1,1 +1,43 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Twins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Number_Of_Coins= int.Parse(Console.ReadLine());
+            string[] P = Console.ReadLine().Split();
+ 
+            int[] Seq_Of_n = new int[Number_Of_Coins];
+ 
+            int sum = 0;
+            for (int i = 0; i < Number_Of_Coins; i++)
+            {
+                Seq_Of_n[i] = int.Parse(P[i]);
+                sum += Seq_Of_n[i];
+            }
+            Array.Sort(Seq_Of_n);
+            Array.Reverse(Seq_Of_n);
+            int sum1 = 0, counter = 0;
+            for (int i = 0; i < Number_Of_Coins; i++)
+            {
+                if(sum1 > sum / 2)
+                {
+                    break;
+                }
+                else
+                {
+                    counter++;
+                    sum1 += Seq_Of_n[i];
+                }
+            }
+            Console.WriteLine(counter);
+ 
+        }
+    }
+}
