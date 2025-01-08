836d1cb4a131f33056ff8f0e4eceaea5297ece2b
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:31:48 +0200

    Update 318_A_Even Odds.cs

--- Codeforces_Solutions/318_A_Even Odds/318_A_Even Odds.cs
+++ Codeforces_Solutions/318_A_Even Odds/318_A_Even Odds.cs
@@ -1,1 +1,34 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Even_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            long n = long.Parse(input[0]);
+            long k = long.Parse(input[1]); // ا1) لفهرس المطلوب-based index)
+ 
+ 
+            // حساب عدد الأعداد الفردية
+            long oddCount = (n + 1) / 2;
+ 
+            if(k <= oddCount)
+            {
+                // الرقم فردي
+                Console.WriteLine(1 + (k-1) *2 );
+            }
+            else
+            {
+                // الرقم زوجي
+                Console.WriteLine(2 + (k-oddCount-1) *2 );
+            }
+ 
+        }
+    }
+}
