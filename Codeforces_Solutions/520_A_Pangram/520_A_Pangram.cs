5048bff99f9ff8637d14adf050582df0ccb95639
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:00:30 +0200

    Update 520_A_Pangram.cs

--- Codeforces_Solutions/520_A_Pangram/520_A_Pangram.cs
+++ Codeforces_Solutions/520_A_Pangram/520_A_Pangram.cs
@@ -1,1 +1,37 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Pangram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            string seqLetters = Console.ReadLine();
+ 
+            seqLetters = seqLetters.ToLower();
+ 
+            HashSet<char> uniqueLetters = new HashSet<char>();
+            foreach(char c in seqLetters)
+            {
+                if (char.IsLetter(c)) 
+                {
+                    uniqueLetters.Add(c);
+                }
+            }
+ 
+            if(uniqueLetters.Count == 26)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
