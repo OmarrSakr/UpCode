7074774ad9a841da9c941f97a7a8139c85dcd27e
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:27:29 +0200

    Update 112_A_Petya and Strings.cs

--- Codeforces_Solutions/112_A_Petya and Strings/112_A_Petya and Strings.cs
+++ Codeforces_Solutions/112_A_Petya and Strings/112_A_Petya and Strings.cs
@@ -1,1 +1,36 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firststring=Console.ReadLine();
+            firststring= firststring.ToLower();
+            string secondstring=Console.ReadLine();
+            secondstring = secondstring.ToLower();
+ 
+            int verify = string.Compare(firststring,secondstring);
+ 
+            if(verify > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if(verify < 0)
+            {
+                Console.WriteLine("-1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+ 
+ 
+        }
+    }
+}
