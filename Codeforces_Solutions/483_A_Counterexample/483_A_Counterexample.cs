4a71602f5b922b6ce779879eac763a22bf95d7a5
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:21:24 +0200

    Update 483_A_Counterexample.cs

--- Codeforces_Solutions/483_A_Counterexample/483_A_Counterexample.cs
+++ Codeforces_Solutions/483_A_Counterexample/483_A_Counterexample.cs
@@ -1,1 +1,46 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace A.Counterexample
+{
+    class Program
+    {
+        static long GCD(long x , long y)
+        {
+            while (y != 0)
+            {
+                long temp = y;
+                y = x % y;
+                x = temp;
+            }
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            long l = long.Parse(input[0]);
+            long r = long.Parse(input[1]);
+ 
+            for(long a = l; a <= r - 2; a++)
+            {
+                for (long b = a+1; b <= r - 1; b++)
+                {
+                    for (long c = b+1; c <= r ; c++)
+                    {
+                        if(GCD(a,b) ==1 && GCD(b,c) == 1 && GCD(a,c) != 1)
+                        {
+                            Console.WriteLine($"{a} {b} {c}");
+                            return;
+                        }
+                    }
+                }
+            }
+            // If no solution found
+            Console.WriteLine("-1");
+ 
+        }
+    }
+}
