7faf322b9c7dd1602df7a8276a2b95d004d96a6f
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 19:53:01 +0200

    Update 731_A_Night at the Museum.cs

--- Codeforces_Solutions/731_A_Night at the Museum/731_A_Night at the Museum.cs
+++ Codeforces_Solutions/731_A_Night at the Museum/731_A_Night at the Museum.cs
@@ -1,1 +1,31 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameexh = Console.ReadLine().Trim().ToLower();
+            int totalroatation = 0;
+            int CurrentPosition = 'a';
+ 
+           foreach(char targetchar in nameexh)
+            {
+                //with ClockWise 
+                int ClockWiseDistance = (targetchar - CurrentPosition + 26) % 26; //i put (+26) and (%26) if the result is nagative number 
+                // Counter ClockWise
+                int CounterClockWiseDistance = (CurrentPosition - targetchar + 26) % 26;
+ 
+                totalroatation += Math.Min(ClockWiseDistance, CounterClockWiseDistance);
+                CurrentPosition = targetchar;
+ 
+            }
+            Console.WriteLine(totalroatation);
+        }
+    }
+}
