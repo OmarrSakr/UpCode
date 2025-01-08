fad6b2a6e349f739b37ac30f0dce380005084740
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:12:10 +0200

    Update 228_A_Is your horseshoe on the other hoof_.cs

--- Codeforces_Solutions/228_A_Is your horseshoe on the other hoof_/228_A_Is your horseshoe on the other hoof_.cs
+++ Codeforces_Solutions/228_A_Is your horseshoe on the other hoof_/228_A_Is your horseshoe on the other hoof_.cs
@@ -1,1 +1,28 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] records = Console.ReadLine().Split(' ');
+            int[] horseshoes = new int[4];
+            for (int i = 0; i < 4; i++)
+            {
+                horseshoes[i] = int.Parse(records[i]);
+            }
+            int need = 0;
+ 
+            // استخدام HashSet لتخزين الألوان الفريدة
+            var uniquecolor =new HashSet<int>(horseshoes);
+ 
+            need=4 - uniquecolor.Count;
+            Console.WriteLine(need);
+        }
+    }
+}
