13e156cce10158d856f8429cf0db5033e5fef7aa
Omar Hussein <omarrhussein13@gmail.com>
2025-01-08 20:15:58 +0200

    Update 266_A_Stones on the Table.cs

--- Codeforces_Solutions/266_A_Stones on the Table/266_A_Stones on the Table.cs
+++ Codeforces_Solutions/266_A_Stones on the Table/266_A_Stones on the Table.cs
@@ -1,1 +1,36 @@
-Code not available
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace cf12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+ 
+           string colors = Console.ReadLine().ToUpper();
+            int problem = 0;
+            if(colors.Length == n)
+            {
+                for (int i = 0; i < colors.Length-1; i++)
+                {
+                    if(colors[i] ==colors[i+1])
+                    {
+                        problem++;
+                    }
+ 
+                }
+            }
+            else
+            {
+                return;
+            }
+           
+            Console.WriteLine(problem);
+        }
+    }
+}
